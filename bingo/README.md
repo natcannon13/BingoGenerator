4646b101f1e7849597697bd7a6f0c6f23cc68a28
Nat Cannon <natcannon13@gmail.com>
2026-03-30 02:04:13 -0600

    Initial Commit
    Project Generated + Help Menu

---
+++ bingo/README.md
@@ -0,0 +1,8 @@
+# Bingo Card Generator [WIP]
+
+A generator for custom bingo cards. 
+2 modes:
+Simple:
+Creates a simple random bingo card. Generates a random 5x5 card with a free space in the middle.
+Balanced:
+Generates a balanced 5x5 card such that each row, column, and diagonal is equally difficult based on user-scored goals. 
---
+++ bingo/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import BingoCard from './components/BingoCard.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <BingoCard/>
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
